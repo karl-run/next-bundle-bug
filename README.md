1975dbbab59b8d7c6e5c317413c480286a18ed63
Karl J. Overå <k@rl.run>
2023-02-01 10:13:50 +0100

    bug repro

---
+++ README.md
@@ -0,0 +1,3 @@
+# Repro of NextJS including too new language features in output bundle
+
+Repro repo for Next.JS issue [#45473](https://github.com/vercel/next.js/issues/45473).
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Uncomment to "fix" the output bundle
+  // transpilePackages: ['filter-obj']
+}
+
+module.exports = nextConfig
